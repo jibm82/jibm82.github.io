2e3e7b687145ec89c9a6bbaef27266b061b4a4f4
Jorge Baez <jibm_82@yahoo.com.mx>
2018-10-20 01:00:04 -0500

    Implemented contact form email

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,20 +2,36 @@ $('.cover').paroller();
 
 $("#contact-form").submit((e) => {
   e.preventDefault();
-  let myEmail = "jibm_82@yahoo.com.mx";
-  let name = $("#name").val().trim();
-  let email = $("#email").val().trim();
-  let message = $("#message").val().trim();
-  let emailBody = `Name: ${name}
-    Email: ${email}
-    Message: ${message}
-  `;
 
-  Email.send(
-    myEmail,
-    myEmail,
-    `New contact form message from ${name} - ${email}`,
-    message,
-    { token: "659fd5c2-77ab-4c22-ab73-27050299be9b" }
-  );
+  const form = $(e.currentTarget);
+
+  if (!form.hasClass("disabled")) {
+    form.addClass("disabled");
+
+    let myEmail = "jibm_82@yahoo.com.mx";
+
+    let name = $("#name").val().trim();
+    let email = $("#email").val().trim();
+    let message = $("#message").val().trim();
+
+    Email.send(
+      myEmail,
+      myEmail,
+      `New contact form message from ${name} - ${email}`,
+      message,
+      {
+        token: "68497cd5-c484-4aab-9818-0154648a69c5",
+        callback: function done(message) {
+          $(".form-control").val("");
+          swal({
+            type: "success",
+            title: "Message sent!",
+            text: "Thank you for contact me, I will get back to you ASAP!"
+          });
+          form.removeClass("disabled");
+        }
+      }
+    );
+  }
 });
+
