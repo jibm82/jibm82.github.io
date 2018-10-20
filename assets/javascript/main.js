23ea5c40a461f041e4a6ec2a454218f14c0145e4
Jorge Baez <jibm_82@yahoo.com.mx>
2018-10-19 23:35:47 -0500

    Test sendjs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,21 @@
 $('.cover').paroller();
+
+$("#contact-form").submit((e) => {
+  e.preventDefault();
+  let myEmail = "jibm_82@yahoo.com.mx";
+  let name = $("#name").val().trim();
+  let email = $("#email").val().trim();
+  let message = $("#message").val().trim();
+  let emailBody = `Name: ${name}
+    Email: ${email}
+    Message: ${message}
+  `;
+
+  Email.send(
+    myEmail,
+    myEmail,
+    `New contact form message from ${name} - ${email}`,
+    message,
+    { token: "659fd5c2-77ab-4c22-ab73-27050299be9b" }
+  );
+});
